afb7854f051b9d419a9c7d506a4f27f14eed8fd2
Joshua Atmadja <joshuatmadja@gmail.com>
2017-09-15 22:01:51 +0700

    source code java update

--- src/myGeneral.java
+++ src/myGeneral.java
@@ -1,7 +1,3 @@
-/**
- * Created by ASUS on 15/09/2017.
- */
-
 import weka.core.Instances;
 
 import java.io.BufferedReader;
--- src/myID3.java
+++ src/myID3.java
@@ -1,5 +1,2 @@
-/**
- * Created by ASUS on 15/09/2017.
- */
 public class myID3 extends myGeneral {
 }
