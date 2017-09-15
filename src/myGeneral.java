c34e6178b12d99a2c322e14b608cdc9d1fa469ea
Joshua Atmadja <joshuatmadja@gmail.com>
2017-09-15 21:53:26 +0700

    creation of myGeneral

--- src/myGeneral.java
+++ src/myGeneral.java
@@ -7,7 +7,7 @@ import weka.core.Instances;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class myC45 extends myGeneral {
+public class myGeneral {
 
     public void readData() {
 
--- src/myID3.java
+++ src/myID3.java
@@ -1,5 +1,5 @@
 /**
  * Created by ASUS on 15/09/2017.
  */
-public class myID3 {
+public class myID3 extends myGeneral {
 }
