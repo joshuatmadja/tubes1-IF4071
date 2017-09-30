bedaa3e663e24bbb9effdd996ea92981b187e4b1
Hasna <hasnurk@gmail.com>
2017-09-30 15:57:18 +0700

    Main peogram functions

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,16 +1,19 @@
+import java.util.Random;
 import weka.classifiers.Classifier;
 import weka.classifiers.evaluation.Evaluation;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.supervised.instance.Resample;
+import weka.classifiers.trees.J48;
 
 public class MainClass {
 
-    Instances train;
-    Instances test;
+    private static Instances train;
+    private static Instances test;
+    private static J48 j48;
 
-    public Instances readFile(String filename) throws Exception {
+    public static Instances readFile(String filename) throws Exception {
         DataSource source = new DataSource(filename);
         Instances data = source.getDataSet();
         // setting class attribute if the data format does not provide this information
@@ -20,7 +23,7 @@ public class MainClass {
         return data;
     }
 
-    public Instances filterResample(Instances data) throws Exception {
+    public static Instances filterResample(Instances data) throws Exception {
         Resample filter = new Resample();
         filter.setBiasToUniformClass(1.0);
 
@@ -33,13 +36,67 @@ public class MainClass {
     }
 
 
-    public void crossValidateEval(Classifier cls, int folds) {
-        // Evaluation eval = new Evaluation();
-        // eval.crossValidateModel(cls, );
+    public static void crossValidation(Classifier cls, int folds) throws Exception{
+        Evaluation eval = new Evaluation(train);
+        eval.crossValidateModel(cls, train, folds, new Random(1));
+        printEval(eval);
     }
 
-    public void preprocessFile(String fileName) throws Exception {
+    public static void percentageSplit (Classifier cls) throws Exception{
+        //random dataset
+        train.randomize(new Random(0));
+        // 80% split
+        int trainSize = (int) Math.round(train.numInstances() * 80 / 100);
+        int testSize = train.numInstances() - trainSize;
+        train = new Instances(train, 0, trainSize);
+        test = new Instances(train, trainSize, testSize);
+
+        Evaluation eval = new Evaluation(train);
+        eval.evaluateModel(cls, test);
+        printEval(eval);
+
+    }
+
+    public static void trainingTest (Classifier cls) throws Exception{
+        Evaluation eval = new Evaluation(train);
+        eval.evaluateModel(cls, train);
+        printEval(eval);
+
+    }
+
+    public static void printEval(Evaluation E) throws Exception {
+        System.out.println(E.toSummaryString("\nResults\n======\n", false));
+        System.out.println(E.toClassDetailsString());
+        System.out.println(E.toMatrixString());
+    }
+
+    public static void printAccuracy(Evaluation E) throws Exception {
+        System.out.println(E.correct());
+    }
+
+    public static void preprocessFile(String fileName) throws Exception {
         Instances data = readFile(fileName);
         Instances filteredData = filterResample(data);
+    }
+
+    public static void saveModel(Classifier cls, String filename) throws Exception{
+        weka.core.SerializationHelper.write("../" + filename, cls);
+    }
+
+    public static Classifier loadModel(String filename) throws Exception{
+        return (Classifier) weka.core.SerializationHelper.read(filename);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        MyID3 ID3;
+        MyC45 C45;
+        j48 = new J48();
+        Classifier cls = j48;
+
+        preprocessFile("../weather.nominal.arff");
+        crossValidation(cls, 10);
+
     }
 }
